9c9be7a3a38c0f7adf4821bc3ff80e2c0ca0f732
Kanad Pardeshi <kanadpardeshi@cse.iitb.ac.in>
2020-09-26 13:39:41 +0000

    Task 2B

--- Task2-Testcases/viewWithColor.sh
+++ Task2-Testcases/viewWithColor.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
  # shellcheck source=./resources/defineColors.sh
 source ./resources/defineColors.sh
-awk -F, '{$RED_BACKGROUND; print $0; $RESET_ALL}' viewWithoutColor.out
+
+echo "$BLACK_BACKGROUND $WHITE_FONT hello! $RESET_ALL";
+awk '
+			BEGIN {FS=","; RS="\r\n";}
+			{if(NR>1) {fontcol[$1]=$3"_FONT"; backcol[$1]=$4"_BACKGROUND";}}
+			END { for (i in fontcol) {
+							print "'$fontcol[i]'", "'$backcol[i]'";
+					}
+			}' $2
