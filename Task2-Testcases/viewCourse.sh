0c4a10600632e0bef5b590490d26a18df08735a9
danish-angural <danishangural28@gmail.com>
2020-09-28 17:06:23 +0530

    corrections

--- Task2-Testcases/viewCourse.sh
+++ Task2-Testcases/viewCourse.sh
@@ -4,4 +4,6 @@ if (grep -q -F "(B[m" $1);
 then a=4; 
 fi;
 b=$(($a+1));
-sed -n "/$2/p" "$1"|sort -k $a,$b;
+echo $2;
+awk 'NR<4' "$1"
+awk -v a=$a -v b=$b -v s="$2" '$a FS $b~s&&NR>3 { print $0;}' "$1" | sort -k $a,$b;
--- Task2-Testcases/viewSemester.sh
+++ Task2-Testcases/viewSemester.sh
@@ -4,4 +4,5 @@ if (grep -q -F "(B[m" $1);
 then a=4; 
 fi;
 b=$(($a+1));
-awk '/'$2'/&&/'$3'/ {print $0;}' "$1" | sort -k $a,$b;
+awk 'NR<4' "$1"
+awk '/'"$2"'/&&/'"$3"'/&&NR>1 {print $0;}' "$1" | sort -k $a,$b;
